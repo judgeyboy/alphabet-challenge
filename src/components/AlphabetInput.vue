464d56b35a81c406f5445e603276ff9abfdcc990
Ben Weber <bweber89@gmail.com>
2017-03-11 10:39:57 +0100

    Initial commit

---
+++ src/components/AlphabetInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <label>{{letter}}</label>
+    <input type="text" v-model="input" @keyup="checkInput" focused>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AlphabetInput',
+    data(){
+      return {
+        input: ''
+      }
+    },
+    props: ['letter', 'onMatch'],
+    methods: {
+      checkInput(){
+        if (this.input === this.letter){
+          this.onMatch();
+          this.input = '';
+        } else {
+          this.input = '';
+        }
+      }
+    }
+  }
+</script>
---
+++ src/components/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <h1>The Alphabet-on-Keyboard Challenge!</h1>
+
+    <div v-show="isStopped">
+      <p>Warm up your fingers! when you're ready...click start</p>
+      <button type="button" @click="startChallenge">START CHALLENGE</button>
+    </div>
+
+    <div v-show="isInProgress">
+      <alphabet-input :letter="letter" :onMatch="checkProgress" />
+    </div>
+
+    <div v-show="isCompleted">
+      <h2>Congratulations!</h2>
+      <p>You've completed the Alphabet-on-Keyboard challenge in {{ completedTimeInSeconds }} seconds.</p>
+      <button type="button" @click="restartGame">Play Again</button>
+    </div>
+
+    {{currentTimeInSeconds}}
+  </div>
+</template>
+
+<script>
+  const ALPHABET = "abcdefgh".split('');
+
+  import AlphabetInput from './AlphabetInput.vue'
+
+  export default {
+    name: 'App',
+    data(){
+      return {
+        abc: '',
+        gameState: 'stopped',
+        interval: null,
+        timeInHundredthOfASecond: 0,
+        startTime: null,
+        endTime: null,
+        letter: 'a'
+      }
+    },
+    computed: {
+      isStopped(){
+        return this.gameState === 'stopped';
+      },
+      isInProgress(){
+        return this.gameState === 'inProgress';
+      },
+      isCompleted(){
+        return this.gameState === 'completed';
+      },
+      completedTimeInSeconds(){
+        return (this.endTime - this.startTime) / 1000 ;
+      },
+      currentTimeInSeconds(){
+        return this.timeInHundredthOfASecond / 10;
+      }
+    },
+    methods: {
+      restartGame(){
+        this.gameState = 'stopped';
+        this.timeInHundredthOfASecond = 0;
+        this.startTime = null;
+        this.endTime = null;
+        this.letter = 'a';
+      },
+      startChallenge(){
+        this.startTime = Date.now();
+        this.timer();
+        this.gameState = 'inProgress';
+      },
+      checkProgress(){
+        let nextLetter = this.getNextLetter(this.letter);
+        if(!nextLetter){
+          this.endTime = Date.now();
+          clearInterval(this.interval);
+          this.gameState = 'completed';
+        } else {
+          this.letter = nextLetter;
+        }
+      },
+      getNextLetter(currentLetter){
+        // todo: handle z (last letter of alphabet case)
+        return ALPHABET[ALPHABET.indexOf(this.letter) + 1];
+      },
+      timer(){
+        this.interval = setInterval(this.updateTime, 100);
+      },
+      updateTime(){
+        this.timeInHundredthOfASecond++;
+      },
+
+    },
+    components: {
+      AlphabetInput
+    }
+
+  }
+</script>
